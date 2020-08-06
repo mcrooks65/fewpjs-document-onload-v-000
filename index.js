09ef848b93a23063de90f2a9cfb4a183b3695127
Matthew Crooks <mfcrooks@gmail.com>
2020-08-06 18:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Your code goes here
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
-});
-console.log("First or second..")
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
+
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
